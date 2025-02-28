037e005ae5fbcc7030cce13859d3092d098b3f0d
Laurentiu1704 <scutariulaur@gmail.com>
2025-02-28 15:46:59 +0100

    init

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB; // Gebruik Query Builder
+use App\Models\Planet; // Gebruik het Planet model
 
 class PlanetController extends Controller
 {
     public function index()
     {
-        $planets = DB::table('planets')->get(); // Haal data op zonder Eloquent
-        dd($planets);
-        return view('planets', compact('planets'));
+        $planets = Planet::all(); // Haal alle planeten op met Eloquent
+        return view('planets', compact('planets')); 
     }
 }
-
-
